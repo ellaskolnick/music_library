e24f1aa0dc8fc27eaa5644ddc173e33cd4e55f93
Ella Skolnick <ellaskolnick@me.com>
2022-10-07 17:31:44 +0100

    done

--- app.rb
+++ app.rb
@@ -1,28 +1,76 @@
 # file: app.rb
 
-require_relative 'lib/database_connection'
-require_relative 'lib/artist_repository'
-require_relative 'lib/album_repository'
+require_relative './lib/album_repository'
+require_relative './lib/artist_repository'
+require_relative './lib/database_connection'
 
-# We need to give the database name to the method `connect`.
-DatabaseConnection.connect('music_library')
+class Application
 
-artist_repository = ArtistRepository.new
-album_repository = AlbumRepository.new
+  # The Application class initializer
+  # takes four arguments:
+  #  * The database name to call `DatabaseConnection.connect`
+  #  * the Kernel object as `io` (so we can mock the IO in our tests)
+  #  * the AlbumRepository object (or a double of it)
+  #  * the ArtistRepository object (or a double of it)
+  def initialize(database_name, io, album_repository, artist_repository)
+    DatabaseConnection.connect(database_name)
+    @io = io
+    @album_repository = album_repository
+    @artist_repository = artist_repository
+  end
 
+  def run
+    show "Welcome to the music library manager!"
+    show "What would you like to do?"
+    show " 1 - List all albums"
+    show " 2 - List all artists"
+    choice = prompt "Enter your choice: "
 
-# Perform a SQL query on the database and get the result set.
-# sql = 'SELECT id, title FROM albums;'
-# result = DatabaseConnection.exec_params(sql, [])
+    if choice == "1"
+      show "Here is the list of albums:"
+      list_albums
+    elsif choice == "2"
+      show "Here is the list of artists:"
+      list_artists
+    end
+  end
 
-# Print out each record from the result set .
+  private
 
-# artist_repository.all.each do |artist|
-#   p artist
-# end
+  def show(message)
+    @io.puts(message)
+  end
 
-# album_repository.all.each do |album|
-#   p album
-# end
+  def prompt(message)
+    @io.puts(message)
+    @io.gets.chomp
+  end
 
-p album_repository.find(3)
+  def list_albums
+    albums = @album_repository.all
+    albums.each_with_index do |album, i|
+      show " * #{i+1} - #{album.title}"
+    end
+  end
+
+  def list_artists
+    artists = @artist_repository.all
+    artists.each_with_index do |artist, i|
+      show " * #{i+1} - #{artist.name}"
+    end
+  end
+end
+
+# Don't worry too much about this if statement. It is basically saying "only
+# run the following code if this is the main file being run, instead of having
+# been required or loaded by another file.
+# If you want to learn more about __FILE__ and $0, see here: https://en.wikibooks.org/wiki/Ruby_Programming/Syntax/Variables_and_Constants#Pre-defined_Variables
+if __FILE__ == $0
+  app = Application.new(
+    'music_library',
+    Kernel,
+    AlbumRepository.new,
+    ArtistRepository.new
+  )
+  app.run
+end
---
+++ spec/app_spec.rb
@@ -0,0 +1,29 @@
+require './app'
+
+RSpec.describe Application do
+  it "lists albums" do
+    io = double(:io)
+    album = double(:album, title: "Doolittle")
+    album2 = double(:album, title: "Surfer Rosa")
+    album3 = double(:album, title: "Waterloo")
+    albums = [album, album2, album3]
+    album_repository = double(:album_repository, all: albums)
+    artist = double(:artist, name: "Pixies")
+    artist2 = double(:artist, name: "ABBA")
+    artist3 = double(:artist, name: "Taylor Swift")
+    artists = [artist, artist2, artist3]
+    artist_repository = double(:artist_repository, all: artists)
+    app = Application.new('music_library_test', Kernel, album_repository, artist_repository)
+    expect(io).to receive(:puts).with("Welcome to the music library manager!")
+    expect(io).to receive(:puts).with("What would you like to do?")
+    expect(io).to receive(:puts).with(" 1 - List all albums")
+    expect(io).to receive(:puts).with(" 2 - List all artists")
+    expect(io).to receive(:puts).with("Enter your choice: ")
+    expect(io).to receive(:gets).and_return("1\n")
+    expect(io).to receive(:puts).with("Here is the list of albums:")
+    expect(io).to receive(:puts).with(" * 1 - Doolittle")
+    expect(io).to receive(:puts).with(" * 2 - Surfer Rosa")
+    expect(io).to receive(:puts).with(" * 3 - Waterloo")
+    app.run
+  end
+end
